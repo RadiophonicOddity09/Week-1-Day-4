5fde10876ae3f436fb3196987fbb4291523b13ec
RadiophonicOddity09 <charlesokwechime@gmail.com>
2018-05-10 21:19:09 +0000

    created a closure that returns the output of a given argument

---
+++ wraplogger.js
@@ -0,0 +1,27 @@
+var wrapLog = function (callback, name) {
+  return function(a, b, c) {
+    if(!c) {
+      console.log(name + "( " + a + ", " + b + ") => " + callback(a, b));
+    } else if(c) {
+      console.log(name + "( " + a + ", " + b + c + ") => " +
+        callback (a, b, c));
+
+    }
+  };
+};
+
+var area = function (x, y) {
+  return x * y;
+};
+var logArea = wrapLog(area, "area");
+
+logArea(5, 3); // area(5, 3) => 15
+logArea(3, 2); // area(3, 2) => 6
+
+var volume = function (x, y, z) {
+  return x * y * z;
+};
+var logVolume = wrapLog(volume, "volume");
+
+logVolume(5, 3, 2); // volume(5, 3, 2) => 30
+logVolume(3, 2, 4); // volume(3, 2, 4) => 24
